d773ac08742fa875311e8af8b8f3b0e4dc6a50fa
Charles Brindle <ash@blueashes.com>
2018-12-13 10:41:33 -0700

    Added Bezier decorator class

---
+++ Assets/MoveAlongBezier/Scripts/SplineWalker.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace CleverCrow.Curves {
+    public class SplineWalker : MonoBehaviour {
+        public BezierSpline spline;
+        public float duration = 5;
+        public bool lookForward;
+
+        private float _progress;
+
+        private void Update () {
+            _progress += Time.deltaTime / duration;
+            
+            if (_progress > 1f) {
+                _progress = 1f;
+            }
+
+            var pos = spline.GetPoint(_progress);
+            transform.localPosition = pos;
+            if (lookForward) {
+                transform.LookAt(pos + spline.GetDirection(_progress));
+            }
+        }
+    }
+}
