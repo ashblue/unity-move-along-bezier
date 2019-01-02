c787f43cb96a6201af99a54212724069c2fd9533
Charles Brindle <ash@blueashes.com>
2019-01-02 12:13:16 -0700

    Points can now be added at the beginning or ending

--- Assets/MoveAlongBezier/Scripts/Curves/FreeCurve/Editor/FreeCurveInspector.cs
+++ Assets/MoveAlongBezier/Scripts/Curves/FreeCurve/Editor/FreeCurveInspector.cs
@@ -14,17 +14,29 @@ namespace CleverCrow.Curves.Editors {
         }
 
         private void AddPoint () {
-            if (!GUILayout.Button("Add Point")) return;
+            if (GUILayout.Button("Add Point Begin")) {
+                var beginPos = _curve.transform.InverseTransformPoint(_curve.Points[0].GlobalPosition);
+                var point = new CurvePoint {
+                    Position = beginPos + Vector3.left * 4,
+                    transform = _curve.transform
+                };
             
-            var endPos = _curve.transform.InverseTransformPoint(_curve.Points[_curve.Points.Count - 1].GlobalPosition);
-            var point = new CurvePoint {
-                Position = endPos + (Vector3.right * 4),
-                transform = _curve.transform
-            };
+                Undo.RecordObject(_curve, "Add Point");
+                EditorUtility.SetDirty(_curve);
+                _curve.Points.Insert(0, point);
+            }
             
-            Undo.RecordObject(_curve, "Add Point");
-            EditorUtility.SetDirty(_curve);
-            _curve.Points.Add(point);
+            if (GUILayout.Button("Add Point End")) {
+                var endPos = _curve.transform.InverseTransformPoint(_curve.Points[_curve.Points.Count - 1].GlobalPosition);
+                var point = new CurvePoint {
+                    Position = endPos + Vector3.right * 4,
+                    transform = _curve.transform
+                };
+            
+                Undo.RecordObject(_curve, "Add Point");
+                EditorUtility.SetDirty(_curve);
+                _curve.Points.Add(point);
+            }
         }
     }
 }
